afef89f16d969034b3a24836ace04589cbae4458
willymwanganje <willymwanganje@gmail.com>
2026-08-26 14:00:32 +0300

    Fix category folder access for authorized users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,14 +20,15 @@ import "./App.css";
 import "./Categories_cards.css";
 import "./Folders_cards.css";
 
-
 export default function App() {
   return (
     <Routes>
+      {/* Public routes */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
 
+      {/* All application routes require authentication */}
       <Route
         element={
           <ProtectedRoute>
@@ -37,23 +38,118 @@ export default function App() {
       >
         <Route path="/" element={<DashboardPage />} />
 
-        <Route path="/documents" element={<ProtectedRoute permission="document.view"><DocumentsPage /></ProtectedRoute>} />
-        <Route path="/documents/mine" element={<ProtectedRoute permission="document.view"><DocumentsPage mine /></ProtectedRoute>} />
-        <Route path="/documents/upload" element={<ProtectedRoute permission="document.create"><UploadDocumentPage /></ProtectedRoute>} />
-        <Route path="/documents/:id" element={<ProtectedRoute permission="document.view"><DocumentDetailsPage /></ProtectedRoute>} />
+        {/* Documents */}
+        <Route
+          path="/documents"
+          element={
+            <ProtectedRoute permission="document.view">
+              <DocumentsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/documents/mine"
+          element={
+            <ProtectedRoute permission="document.view">
+              <DocumentsPage mine />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/documents/upload"
+          element={
+            <ProtectedRoute permission="document.create">
+              <UploadDocumentPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/documents/:id"
+          element={
+            <ProtectedRoute permission="document.view">
+              <DocumentDetailsPage />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="/categories" element={<ProtectedRoute permission="category.view"><CategoriesPage /></ProtectedRoute>} />
-        <Route path="/categories/:categoryId" element={<ProtectedRoute permission="category.view"><FoldersPage /></ProtectedRoute>} />
-        <Route path="/categories/:categoryId/folders/:id" element={<ProtectedRoute permission="folder.view"><FoldersPage /></ProtectedRoute>} />
+        {/* Categories and category-owned folders */}
+        <Route
+          path="/categories"
+          element={
+            <ProtectedRoute permission="category.view">
+              <CategoriesPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/categories/:categoryId"
+          element={
+            <ProtectedRoute permission="category.view">
+              <FoldersPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/categories/:categoryId/folders/:id"
+          element={
+            <ProtectedRoute permission="category.view">
+              <FoldersPage />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="/folders" element={<ProtectedRoute permission="folder.view"><FoldersPage /></ProtectedRoute>} />
-        <Route path="/folders/:id" element={<ProtectedRoute permission="folder.view"><FoldersPage /></ProtectedRoute>} />
+        {/* General folder library */}
+        <Route
+          path="/folders"
+          element={
+            <ProtectedRoute permission="folder.view">
+              <FoldersPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/folders/:id"
+          element={
+            <ProtectedRoute permission="folder.view">
+              <FoldersPage />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="/users" element={<ProtectedRoute permission="user.view"><UsersPage /></ProtectedRoute>} />
+        {/* User and administration pages */}
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute permission="user.view">
+              <UsersPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/users/:id" element={<UserDetailsPage />} />
-        <Route path="/administrators" element={<ProtectedRoute permission="admin.view"><UsersPage adminMode /></ProtectedRoute>} />
-        <Route path="/roles" element={<ProtectedRoute permission="role.view"><RolesPage /></ProtectedRoute>} />
-        <Route path="/audit-logs" element={<ProtectedRoute permission="audit.view"><AuditLogsPage /></ProtectedRoute>} />
+        <Route
+          path="/administrators"
+          element={
+            <ProtectedRoute permission="admin.view">
+              <UsersPage adminMode />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/roles"
+          element={
+            <ProtectedRoute permission="role.view">
+              <RolesPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/audit-logs"
+          element={
+            <ProtectedRoute permission="audit.view">
+              <AuditLogsPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/settings" element={<SettingsPage />} />
       </Route>
